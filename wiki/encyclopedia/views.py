6ba62e7b7f0a0a4421deb6378cd44e168cc3ab07
Juan David <davidvm419@gmail.com>
2026-02-16 10:34:26 -0500

    Add Entry page functionality and specification notes

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -2,9 +2,27 @@ from django.shortcuts import render
 
 from . import util
 
+# Library to convert md to html
+from markdown2 import Markdown
+markdowner = Markdown()
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+# Function to get entries of the wiki
+
+
+def entry(request, title):
+    content = util.get_entry(title)
+    if not content:
+        return render(request,"encyclopedia/error.html", {
+            "title": title.capitalize()
+        })
+    else:
+        html_content = markdowner.convert(content)
+        return render(request, "encyclopedia/entry.html", {
+            "title": title.capitalize(),
+            "content": html_content
+        })
