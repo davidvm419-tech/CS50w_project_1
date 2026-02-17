41bf4f77ba7b7f1fdc9858c2b5d164a9e952ec5e
Juan David <davidvm419@gmail.com>
2026-02-17 10:34:20 -0500

    Add Index page links and Search functionality for complete, partial and no matches

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,10 +1,17 @@
-from django.shortcuts import render
-
+from django import forms
+from django.http import HttpResponseRedirect
+from django.shortcuts import redirect, render
 from . import util
-
 # Library to convert md to html
 from markdown2 import Markdown
 markdowner = Markdown()
+# Regex to get matches on query
+import re
+
+# Class to search query
+class SearchEntry(forms.Form):
+    q = forms.CharField(label="Search Query")
+
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
@@ -26,3 +33,29 @@ def entry(request, title):
             "title": title.capitalize(),
             "content": html_content
         })
+
+
+# Function to get a query from the search bar
+
+
+def search_query(request):
+    if request.method == "POST":
+        form = SearchEntry(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data["q"]
+            entry = util.get_entry(title)
+            if entry:
+                return redirect("entry", title=title)
+            else:
+                entries = util.list_entries()
+                matches = []
+                for entry in entries:
+                    if re.search(title, entry, re.IGNORECASE):
+                        matches.append(entry)
+                return render(request, "encyclopedia/search.html", {
+                    "entries": matches,
+                })
+        else:
+            return render(request, "encyclopedia/index.html", {
+                "entries": util.list_entries()
+            })
